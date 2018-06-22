b38dede113f4d1b7f6981b03f9bf13496d7cba5d
smac89 <smac89@users.noreply.github.com>
2018-06-22 00:48:46 -0600

    Added all the arduino examples to the cmakelists file

--- blinky.ino
+++ blinky.ino
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
-void setup() {
+const static uint8_t ledPin = 10;
 
+void setup() {
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-
+    digitalWrite(ledPin, LOW);
+//    delay(2000);
+//    digitalWrite(ledPin, HIGH);
+//    delay(1300);
 }
